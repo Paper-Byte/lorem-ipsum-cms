73fd0ba54918d01c68cc3cb51f108244a1499635
Noah <noahhughesph@gmail.com>
2023-09-19 16:07:52 -0400

    initializes repo, framework, libraries, packages

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Heading } from "@chakra-ui/react";
+
+const Header = () => {
+    return(
+        <Heading as="h2" size="2xl" noOfLines={1}>Chakra Testing</Heading>
+    )
+}
+
+export default Header;
