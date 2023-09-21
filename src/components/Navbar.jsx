cc0ac28de1ba930d1c60393aa3607e55a3051182
Noah <noahhughesph@gmail.com>
2023-09-21 14:36:32 -0400

    Fix image source in Navbar.jsx and add WelcomeCard component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -56,7 +56,7 @@ export default function WithSubnavigation() {
           justify={{ base: 'center', md: 'start' }}
         >
           <Image
-            src="process.env.REACT_APP_LOGO"
+            src={process.env.REACT_APP_LOGO}
             alt="Marketplace Logo"
             borderRadius="full"
             boxSize="40px"
