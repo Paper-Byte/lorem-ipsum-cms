0b6755aa59d7cfe17bb3d1bbaebab49459c18604
Noah <noahhughesph@gmail.com>
2023-09-21 14:42:51 -0400

    Refactor file structure and component names in Welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Navbar from './components/Navbar.jsx';
-import CreateClothing from './components/CreateClothingForm/CreateClothingBody';
+import CreateClothing from './components/CreateClothingPage/CreateClothingBody';
 import Footer from './components/Footer';
 
 function App() {
